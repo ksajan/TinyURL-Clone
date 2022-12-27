bae0533856594eb9c424cf5867bd119d3b663777
Sajan Kumar <ksajan12@gmail.com>
2022-12-27 09:19:57 +0530

    added fiber routes

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	service "tinyURL/services/url_generator"
+	"os"
+	routes "tinyURL/services/api"
 	logging "tinyURL/utils"
+
+	"github.com/gofiber/fiber/v2"
+	fiberLogger "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
+
+func initRoutes(app *fiber.App) {
+	app.Post("/shorten", routes.ShortenURL)
+	app.Get("/:shortURLEncodedValue", routes.ResolveURL)
+}
 
 func main() {
 
 	logger := logging.NewLogger()
-
+	err := godotenv.Load()
+	if err != nil {
+		logger.Errorln("Error loading .env file")
+	}
 	logger.Infoln("Hello, playground")
-	url := "https://tinyurl.com/api-create.php?url=https%"
-	res := service.GenerateURL(url, "https://tinyurl.com")
-	logger.Infoln(res)
+	app := fiber.New()
+	app.Use(fiberLogger.New())
+	initRoutes(app)
+	logger.Infoln("Listening on post", os.Getenv("APP_PORT"))
+	logger.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
